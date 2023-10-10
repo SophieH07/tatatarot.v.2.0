80cdbc97b5b01c9d7270c44d2d10cdf05ecbbcdd
Sophie <szaniszlozs7@gmail.com>
2023-10-10 23:08:30 +0200

    add generator

--- src/pages/Generator.jsx
+++ src/pages/Generator.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useState } from "react";
+import cardList from "../components/cardList";
 
 const Generator = () => {
-  return <div>Generator</div>;
+  const [randomCard, setRandomCard] = useState(null);
+  const [clicked, setClicked] = useState(false);
+
+  const pickRandomCard = () => {
+    const randomIndex = Math.floor(Math.random() * cardList.length);
+    setRandomCard(cardList[randomIndex]);
+    setClicked(true);
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="justify-center items-center text-center">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
+          onClick={pickRandomCard}
+        >
+          Pick a Random Card
+        </button>
+        {randomCard && (
+          <div className="animate-jump-in animate-duration-[900ms] animate-delay-1000">
+            <p className="font-bold uppercase py-4">{randomCard.name}</p>
+            <img
+              className="max-h-[30em]"
+              src={randomCard.picture}
+              alt={randomCard.name}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Generator;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import Logo from "../assets/tarotlogotrans.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className=" text-center text-5xl uppercase font-bold">
+      <p className="animate-fade-right pt-4">
+        Welcome on Tatatarot! Pick a tarot card!
+      </p>
+      <div className="flex justify-center items-center pt-9">
+        <img
+          className="max-h-[60vh] max-w-[60vh] animate-wiggle animate-infinite"
+          src={Logo}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
