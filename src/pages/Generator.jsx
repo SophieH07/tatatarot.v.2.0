6335c8c51679006430669927df2c80b72f84b3e1
Sophie <szaniszlozs7@gmail.com>
2023-10-12 22:30:17 +0200

    add more generate

--- src/pages/Generator.jsx
+++ src/pages/Generator.jsx
@@ -2,34 +2,58 @@ import { useState } from "react";
 import cardList from "../components/cardList";
 
 const Generator = () => {
-  const [randomCard, setRandomCard] = useState(null);
-  const [clicked, setClicked] = useState(false);
-
-  const pickRandomCard = () => {
-    const randomIndex = Math.floor(Math.random() * cardList.length);
-    setRandomCard(cardList[randomIndex]);
-    setClicked(true);
-  };
+  const [randomCards, setRandomCards] = useState(cardList);
+  const [picked, setPicked] = useState(false);
+  function getRandomCards(number) {
+    const shuffledCards = [...randomCards].sort(() => 0.5 - Math.random());
+    const selected = shuffledCards.slice(0, number);
+    setRandomCards(selected);
+    setPicked(true);
+  }
 
   return (
     <div className="flex justify-center items-center">
       <div className="justify-center items-center text-center">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
-          onClick={pickRandomCard}
-        >
-          Pick a Random Card
-        </button>
-        {randomCard && (
-          <div className="animate-jump-in animate-duration-[900ms] animate-delay-1000">
-            <p className="font-bold uppercase py-4">{randomCard.name}</p>
-            <img
-              className="max-h-[30em]"
-              src={randomCard.picture}
-              alt={randomCard.name}
-            />
+        <div className="inline-flex items-center">
+          <select
+            name="number-cards"
+            id="number-cards"
+            className="p-2 bg-none bg-black rounded bg-opacity-10 text-lg"
+            onChange={(e) => getRandomCards(e.target.value)}
+          >
+            <option className="bg-slate-800" value="0">
+              How many cards?
+            </option>
+            <option className="bg-slate-800" value="1">
+              One
+            </option>
+            <option className="bg-slate-800" value="3">
+              Three
+            </option>
+            <option className="bg-slate-800" value="6">
+              Six
+            </option>
+            <option className="bg-slate-800" value="12">
+              Twelve
+            </option>
+          </select>
+          <div className="pl-2">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
+              onClick={getRandomCards}
+            >
+              Pick Random Cards
+            </button>
           </div>
-        )}
+        </div>
+        <div>
+          {picked &&
+            randomCards.map((c) => (
+              <div key={c.name}>
+                <p>{c.name}</p> <img className="max-w-[14em]" src={c.picture} />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   );
