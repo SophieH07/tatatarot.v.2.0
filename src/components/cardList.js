6d35b0705856c87e5bc14804de695dfa79d0a7f1
Sophie <szaniszlozs7@gmail.com>
2025-10-05 04:14:34 +0200

    refactor images to public, remove all the imports

--- src/components/cardList.js
+++ src/components/cardList.js
@@ -1,577 +1,500 @@
-import C01 from "../assets/cards/C-01.jpg";
-import C02 from "../assets/cards/C-02.jpg";
-import C03 from "../assets/cards/C-03.jpg";
-import C04 from "../assets/cards/C-04.jpg";
-import C05 from "../assets/cards/C-05.jpg";
-import C06 from "../assets/cards/C-06.jpg";
-import C07 from "../assets/cards/C-07.jpg";
-import C08 from "../assets/cards/C-08.jpg";
-import C09 from "../assets/cards/C-09.jpg";
-import C10 from "../assets/cards/C-10.jpg";
-import chariot from "../assets/cards/chariot.jpg";
-import death from "../assets/cards/death.jpg";
-import devil from "../assets/cards/devil.jpg";
-import emperor from "../assets/cards/emperor.jpg";
-import empress from "../assets/cards/empress.jpg";
-import fool from "../assets/cards/fool.jpg";
-import hanged_man from "../assets/cards/hanged_man.jpg";
-import hermit from "../assets/cards/hermit.jpg";
-import hierophant from "../assets/cards/hierophant.jpg";
-import judgement from "../assets/cards/judgement.jpg";
-import justice from "../assets/cards/justice.jpg";
-import PK from "../assets/cards/K-P.jpg";
-import SK from "../assets/cards/K-S.jpg";
-import WK from "../assets/cards/K-W.jpg";
-import CK from "../assets/cards/K-C.jpg";
-import CKN from "../assets/cards/KN-C.jpg";
-import PKN from "../assets/cards/KN-P.jpg";
-import SKN from "../assets/cards/KN-S.jpg";
-import WKN from "../assets/cards/KN-W.jpg";
-import lovers from "../assets/cards/lovers.jpg";
-import magician from "../assets/cards/magician.jpg";
-import moon from "../assets/cards/moon.jpg";
-import P01 from "../assets/cards/P-01.jpg";
-import P02 from "../assets/cards/P-02.jpg";
-import P03 from "../assets/cards/P-03.jpg";
-import P04 from "../assets/cards/P-04.jpg";
-import P05 from "../assets/cards/P-05.jpg";
-import P06 from "../assets/cards/P-06.jpg";
-import P07 from "../assets/cards/P-07.jpg";
-import P08 from "../assets/cards/P-08.jpg";
-import P09 from "../assets/cards/P-09.jpg";
-import P10 from "../assets/cards/P-10.jpg";
-import CP from "../assets/cards/P-C.jpg";
-import PP from "../assets/cards/P-P.jpg";
-import SP from "../assets/cards/P-S.jpg";
-import WP from "../assets/cards/P-W.jpg";
-import priestess from "../assets/cards/priestess.jpg";
-import CQ from "../assets/cards/Q-C.jpg";
-import PQ from "../assets/cards/Q-P.jpg";
-import SQ from "../assets/cards/Q-S.jpg";
-import WQ from "../assets/cards/Q-W.jpg";
-import S01 from "../assets/cards/S-01.jpg";
-import S02 from "../assets/cards/S-02.jpg";
-import S03 from "../assets/cards/S-03.jpg";
-import S04 from "../assets/cards/S-04.jpg";
-import S05 from "../assets/cards/S-05.jpg";
-import S06 from "../assets/cards/S-06.jpg";
-import S07 from "../assets/cards/S-07.jpg";
-import S08 from "../assets/cards/S-08.jpg";
-import S09 from "../assets/cards/S-09.jpg";
-import S10 from "../assets/cards/S-10.jpg";
-import star from "../assets/cards/star.jpg";
-import strength from "../assets/cards/strength.jpg";
-import sun from "../assets/cards/sun.jpg";
-import temperance from "../assets/cards/temperance.jpg";
-import tower from "../assets/cards/tower.jpg";
-import W01 from "../assets/cards/W-01.jpg";
-import W02 from "../assets/cards/W-02.jpg";
-import W03 from "../assets/cards/W-03.jpg";
-import W04 from "../assets/cards/W-04.jpg";
-import W05 from "../assets/cards/W-05.jpg";
-import W06 from "../assets/cards/W-06.jpg";
-import W07 from "../assets/cards/W-07.jpg";
-import W08 from "../assets/cards/W-08.jpg";
-import W09 from "../assets/cards/W-09.jpg";
-import W10 from "../assets/cards/W-10.jpg";
-import wheel_of_fortune from "../assets/cards/wheel_of_fortune.jpg";
-import world from "../assets/cards/world.jpg";
+// image imports commented out because images now live in the public/cards folder
 
 const cardList = [
   {
     name: "chariot",
-    picture: chariot,
+    picture: "/cards/chariot.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
-  { name: "death", picture: death, arcana: "major", upward: "", downward: "" },
-  { name: "devil", picture: devil, arcana: "major", upward: "", downward: "" },
+  { name: "death", picture: "/cards/death.jpg", arcana: "major", upward: "", downward: "" },
+  { name: "devil", picture: "/cards/devil.jpg", arcana: "major", upward: "", downward: "" },
   {
     name: "emperor",
-    picture: emperor,
+    picture: "/cards/emperor.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
   {
     name: "empress",
-    picture: empress,
+    picture: "/cards/empress.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
-  { name: "fool", picture: fool, arcana: "major", upward: "", downward: "" },
+  { name: "fool", picture: "/cards/fool.jpg", arcana: "major", upward: "", downward: "" },
   {
     name: "hanged man",
-    picture: hanged_man,
+    picture: "/cards/hanged_man.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
   {
     name: "hermit",
-    picture: hermit,
+    picture: "/cards/hermit.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
   {
     name: "hierophant",
-    picture: hierophant,
+    picture: "/cards/hierophant.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
   {
     name: "judgement",
-    picture: judgement,
+    picture: "/cards/judgement.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
   {
     name: "justice",
-    picture: justice,
+    picture: "/cards/justice.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
   {
     name: "lovers",
-    picture: lovers,
+    picture: "/cards/lovers.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
   {
     name: "magician",
-    picture: magician,
+    picture: "/cards/magician.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
-  { name: "moon", picture: moon, arcana: "major", upward: "", downward: "" },
+  { name: "moon", picture: "/cards/moon.jpg", arcana: "major", upward: "", downward: "" },
   {
     name: "priestess",
-    picture: priestess,
+    picture: "/cards/priestess.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
-  { name: "star", picture: star, arcana: "major", upward: "", downward: "" },
+  { name: "star", picture: "/cards/star.jpg", arcana: "major", upward: "", downward: "" },
   {
     name: "strength",
-    picture: strength,
+    picture: "/cards/strength.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
-  { name: "sun", picture: sun, arcana: "major", upward: "", downward: "" },
+  { name: "sun", picture: "/cards/sun.jpg", arcana: "major", upward: "", downward: "" },
   {
     name: "temperance",
-    picture: temperance,
+    picture: "/cards/temperance.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
-  { name: "tower", picture: tower, arcana: "major", upward: "", downward: "" },
+  { name: "tower", picture: "/cards/tower.jpg", arcana: "major", upward: "", downward: "" },
   {
     name: "wheel of fortune",
-    picture: wheel_of_fortune,
+    picture: "/cards/wheel_of_fortune.jpg",
     arcana: "major",
     upward: "",
     downward: "",
   },
-  { name: "world", picture: world, arcana: "major", upward: "", downward: "" },
+  { name: "world", picture: "/cards/world.jpg", arcana: "major", upward: "", downward: "" },
   {
     name: "Ace of Cups",
-    picture: C01,
+    picture: "/cards/C-01.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Two of Cups",
-    picture: C02,
+    picture: "/cards/C-02.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Three of Cups",
-    picture: C03,
+    picture: "/cards/C-03.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Four of Cups",
-    picture: C04,
+    picture: "/cards/C-04.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Five of Cups",
-    picture: C05,
+    picture: "/cards/C-05.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Six of Cups",
-    picture: C06,
+    picture: "/cards/C-06.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Seven of Cups",
-    picture: C07,
+    picture: "/cards/C-07.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Eight of Cups",
-    picture: C08,
+    picture: "/cards/C-08.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Nine of Cups",
-    picture: C09,
+    picture: "/cards/C-09.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Ten of Cups",
-    picture: C10,
+    picture: "/cards/C-10.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Page of Cups",
-    picture: CP,
+    picture: "/cards/P-C.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Knight of Cups",
-    picture: CKN,
+    picture: "/cards/KN-C.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Queen of Cups",
-    picture: CQ,
+    picture: "/cards/Q-C.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "King of Cups",
-    picture: CK,
+    picture: "/cards/K-C.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Ace of Pentacles",
-    picture: P01,
+    picture: "/cards/P-01.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Two of Pentacles",
-    picture: P02,
+    picture: "/cards/P-02.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Three of Pentacles",
-    picture: P03,
+    picture: "/cards/P-03.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Four of Pentacles",
-    picture: P04,
+    picture: "/cards/P-04.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Five of Pentacles",
-    picture: P05,
+    picture: "/cards/P-05.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Six of Pentacles",
-    picture: P06,
+    picture: "/cards/P-06.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Seven of Pentacles",
-    picture: P07,
+    picture: "/cards/P-07.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Eight of Pentacles",
-    picture: P08,
+    picture: "/cards/P-08.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Nine of Pentacles",
-    picture: P09,
+    picture: "/cards/P-09.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Ten of Pentacles",
-    picture: P10,
+    picture: "/cards/P-10.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Page of Pentacles",
-    picture: PP,
+    picture: "/cards/P-P.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Knight of Pentacles",
-    picture: PKN,
+    picture: "/cards/KN-P.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Queen of Pentacles",
-    picture: PQ,
+    picture: "/cards/Q-P.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "King of Pentacles",
-    picture: PK,
+    picture: "/cards/K-P.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Ace of Swords",
-    picture: S01,
+    picture: "/cards/S-01.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Two of Swords",
-    picture: S02,
+    picture: "/cards/S-02.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Three of Swords",
-    picture: S03,
+    picture: "/cards/S-03.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Four of Swords",
-    picture: S04,
+    picture: "/cards/S-04.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Five of Swords",
-    picture: S05,
+    picture: "/cards/S-05.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Six of Swords",
-    picture: S06,
+    picture: "/cards/S-06.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Seven of Swords",
-    picture: S07,
+    picture: "/cards/S-07.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Eight of Swords",
-    picture: S08,
+    picture: "/cards/S-08.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Nine of Swords",
-    picture: S09,
+    picture: "/cards/S-09.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Ten of Swords",
-    picture: S10,
+    picture: "/cards/S-10.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Page of Swords",
-    picture: SP,
+    picture: "/cards/P-S.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Knight of Swords",
-    picture: SKN,
+    picture: "/cards/KN-S.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Queen of Swords",
-    picture: SQ,
+    picture: "/cards/Q-S.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "King of Swords",
-    picture: SK,
+    picture: "/cards/K-S.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Ace of Wands",
-    picture: W01,
+    picture: "/cards/W-01.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Two of Wands",
-    picture: W02,
+    picture: "/cards/W-02.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Three of Wands",
-    picture: W03,
+    picture: "/cards/W-03.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Four of Wands",
-    picture: W04,
+    picture: "/cards/W-04.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Five of Wands",
-    picture: W05,
+    picture: "/cards/W-05.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Six of Wands",
-    picture: W06,
+    picture: "/cards/W-06.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Seven of Wands",
-    picture: W07,
+    picture: "/cards/W-07.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Eight of Wands",
-    picture: W08,
+    picture: "/cards/W-08.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Nine of Wands",
-    picture: W09,
+    picture: "/cards/W-09.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Ten of Wands",
-    picture: W10,
+    picture: "/cards/W-10.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Page of Wands",
-    picture: WP,
+    picture: "/cards/P-W.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Knight of Wands",
-    picture: WKN,
+    picture: "/cards/KN-W.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "Queen of Wands",
-    picture: WQ,
+    picture: "/cards/Q-W.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
   },
   {
     name: "King of Wands",
-    picture: WK,
+    picture: "/cards/K-W.jpg",
     arcana: "minor",
     upward: "",
     downward: "",
